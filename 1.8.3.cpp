ce7db8eaf64e66588692e4474b348b8760db5752
valar_morghulis <valar_morghulis>
2016-10-25 13:45:23 +0800

    1

---
+++ 1.8.3.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+	char checksum = -1;
+	char a = getchar();
+	while(a != '\n') {
+		int temp = checksum;
+		checksum = checksum + a;
+		printf("%c", a);
+		a = getchar(); 
+	}
+	checksum = checksum + a;
+	printf("\n");
+	printf("%d", checksum);
+	
+}
